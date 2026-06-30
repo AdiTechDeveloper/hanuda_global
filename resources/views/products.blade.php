0478bdfcc5bfa22189162ddaf516f3c33816e18a
AdiTechDeveloper <aditechdeveloper@gmail.com>
2026-06-30 10:17:16 +0530

    Added dynamic title and minor changes in products page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,11 +3,12 @@
 <div class="page-projects">
     <div class="container">
         <div class="row">
+
+            <!-- Filter Nav -->
             <div class="col-lg-12">
-                <!-- Choose Our Project Nav start -->
                 <div class="our-Project-nav wow fadeInUp" data-wow-delay="0.4s">
                     <ul>
-                        <li><a href="#" class="active-btn" data-filter="*">all</a></li>
+                        <li><a href="#" class="active-btn" data-filter="*">All</a></li>
                         <li><a href="#" data-filter=".bearings">Bearings</a></li>
                         <li><a href="#" data-filter=".electricalmotors">Electrical Motors</a></li>
                         <li><a href="#" data-filter=".beltingbelts">Belting / Belts</a></li>
@@ -18,175 +19,83 @@
                         <li><a href="#" data-filter=".constructionmining">Construction & Mining</a></li>
                     </ul>
                 </div>
-                <!-- Choose Our Project Nav End -->
             </div>
 
+            <!-- Items -->
             <div class="col-lg-12">
-                <!-- Project Item Boxes start -->
-                <div class="row project-item-boxes align-items-center">
-                    <div class="col-md-6 project-item-box bearings automation">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp">
+                <div class="row project-item-boxes">
+
+                    @php
+                    $projects = [
+                    ['class'=>'bearings','img'=>'bearings.jpeg','title'=>'Total Quality Management Implementation','tag'=>'Bearings'],
+                    ['class'=>'electricalmotors','img'=>'electrical-moter.jpeg','title'=>'Advanced Research in Material Science','tag'=>'Electrical Motors'],
+                    ['class'=>'beltingbelts','img'=>'belts.jpeg','title'=>'Workplace Safety Enhancement Initiative','tag'=>'Belting Belts'],
+                    ['class'=>'rollerchains','img'=>'roller-chain.jpeg','title'=>'Robotic Process Automation Deployment','tag'=>'Roller Chains'],
+                    ['class'=>'gearboxes','img'=>'gear-box.jpeg','title'=>'Energy-Efficient Manufacturing Systems','tag'=>'Gear Boxes'],
+                    ['class'=>'casters','img'=>'casters.jpeg','title'=>'Redesigning Factory Layouts for Efficiency','tag'=>'Casters'],
+                    ['class'=>'lubricants','img'=>'lubricants.jpeg','title'=>'Optimized Lubrication Systems','tag'=>'Lubricants'],
+                    ['class'=>'constructionmining','img'=>'construction-mining.jpeg','title'=>'Heavy Equipment Optimization','tag'=>'Construction & Mining'],
+                    ];
+                    @endphp
+
+                    @foreach($projects as $index => $project)
+                    <div class="col-md-6 project-item-box {{ $project['class'] }}">
+                        <div class="project-item wow fadeInUp" data-wow-delay="{{ $index * 0.2 }}s">
                             <div class="project-image">
                                 <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/bearings.jpeg') }}" alt="">
+                                    <img src="{{ asset('assets/images/'.$project['img']) }}" alt="{{ $project['tag'] }}">
                                 </figure>
                             </div>
 
                             <div class="project-tag">
-                                <a href="project-single.html">Bearings</a>
+                                <a href="#">{{ $project['tag'] }}</a>
                             </div>
 
                             <div class="project-content">
-                                <h3><a href="project-single.html">Total Quality Management Implementation</a></h3>
+                                <h3><a href="#">{{ $project['title'] }}</a></h3>
                             </div>
                         </div>
-                        <!-- Project Item End -->
                     </div>
+                    @endforeach
 
-                    <div class="col-md-6 project-item-box electricalmotors sustainability development">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="0.2s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/electrical-moter.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">electricalmotors</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Advanced Research in Material Science</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
-
-                    <div class="col-md-6 project-item-box beltingbelts infrastructure">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="0.4s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/belts.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">Belting Belts</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Workplace Safety Enhancement Initiative</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
-
-                    <div class="col-md-6 project-item-box rollerchains development">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="0.6s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/roller-chain.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">Roller Chains</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Robotic Process Automation Deployment</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
-
-                    <div class="col-md-6 project-item-box gearboxes manufacturing">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="0.8s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/gear-box.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">Gear Boxes</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Energy-Efficient Manufacturing Systems</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
-
-                    <div class="col-md-6 project-item-box casters infrastructure">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="1s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/casters.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">Casters</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Redesigning Factory Layouts for Efficiency</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
-
-                    <div class="col-md-6 project-item-box lubricants infrastructure">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="1s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/lubricants.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">Lubricants</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Redesigning Factory Layouts for Efficiency</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
-
-                    <div class="col-md-6 project-item-box constructionmining infrastructure">
-                        <!-- Project Item Start -->
-                        <div class="project-item wow fadeInUp" data-wow-delay="1s">
-                            <div class="project-image">
-                                <figure class="image-anime">
-                                    <img src="{{ asset('assets/images/construction-&-mining.jpeg') }}" alt="">
-                                </figure>
-                            </div>
-
-                            <div class="project-tag">
-                                <a href="project-single.html">Construction & Mining</a>
-                            </div>
-
-                            <div class="project-content">
-                                <h3><a href="project-single.html">Redesigning Factory Layouts for Efficiency</a></h3>
-                            </div>
-                        </div>
-                        <!-- Project Item End -->
-                    </div>
                 </div>
-                <!-- Project Item Boxes End -->
             </div>
+
         </div>
     </div>
 </div>
+@endsection
+
+
+{{-- ✅ IMPORTANT SCRIPTS --}}
+@section('scripts')
+<script>
+    $(document).ready(function() {
+
+        var $grid = $('.project-item-boxes').isotope({
+            itemSelector: '.project-item-box'
+            , layoutMode: 'fitRows'
+        });
+
+        // Fix layout after images load (MOST IMPORTANT)
+        $grid.imagesLoaded().progress(function() {
+            $grid.isotope('layout');
+        });
+
+        // Filter click
+        $('.our-Project-nav a').on('click', function(e) {
+            e.preventDefault();
+
+            $('.our-Project-nav a').removeClass('active-btn');
+            $(this).addClass('active-btn');
+
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({
+                filter: filterValue
+            });
+        });
+
+    });
+
+</script>
 @endsection
