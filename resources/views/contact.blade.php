f7a86a5d8a63922cf2f4221c27a6941edaa7a8ee
AdiTechDeveloper <aditechdeveloper@gmail.com>
2026-06-30 18:01:39 +0530

    contact page changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,101 +1,28 @@
 @extends('layout.default')
 @section('title', 'Contact')
 @section('content')
-    <!-- Page Header Start -->
-    <div class="page-header parallaxie">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <!-- Page Header Box Start -->
-                    <div class="page-header-box">
-                        <h1 class="text-anime-style-2" data-cursor="-opaque">Contact <span>us</span></h1>
-                        <nav class="wow fadeInUp">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ url('/') }}">home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">contact us</li>
-                            </ol>
-                        </nav>
-                    </div>
-                    <!-- Page Header Box End -->
+<!-- Page Header Start -->
+<div class="page-header parallaxie">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <!-- Page Header Box Start -->
+                <div class="page-header-box">
+                    <h1 class="text-anime-style-2" data-cursor="-opaque">Contact <span>us</span></h1>
+                    <nav class="wow fadeInUp">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('/') }}">home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">contact us</li>
+                        </ol>
+                    </nav>
                 </div>
+                <!-- Page Header Box End -->
             </div>
         </div>
     </div>
-    <!-- Page Header End -->
+</div>
+<!-- Page Header End -->
 
-    <!-- Page Contact Us Start -->
-    <div class="page-contact-us">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-5">
-                    <!-- Contact Information Start -->
-                    <div class="contact-information">
-                        <!-- Section Title Start -->
-                        <div class="section-title">
-                            <h3 class="wow fadeInUp">contact us</h3>
-                            <h2 class="text-anime-style-3" data-cursor="-opaque">Get in touch <span>with us</span></h2>
-                            <p class="wow fadeInUp" data-wow-delay="0.2s">Reach out for any inquiries, support, or to
-                                discuss how we can meet your industrial needs.</p>
-                        </div>
-                        <!-- Section Title End -->
-
-                        <!-- Contact Info Box Start -->
-                        <div class="contact-info-box">
-                            <!-- Page Contact Item Start -->
-                            <div class="contact-info-item wow fadeInUp">
-                                <div class="icon-box">
-                                    <img src="{{ asset('assets/images/icon-phone.svg') }}" alt="">
-                                </div>
-                                <div class="contact-info-content">
-                                    <h3>contact</h3>
-                                    {{-- <p>(+1) 630 339-2658</p>
-                                 <p>(+1) 905-915-0210</p> --}}
-                                  <ul style="list-style: none;">
-                                        <li>
-                                            <a href="tel:+19059150210" class="footer-contact-item">
-                                                <img src="{{ asset('assets/images/canada.svg') }}" alt="Canada">
-                                                <span> (+1) 905-915-0210</span>
-                                            </a>
-                                        </li>
-
-
-                                        <!-- USA Number -->
-                                        <li>
-                                            <a href="tel:+16303392658" class="footer-contact-item">
-                                                <img src="{{ asset('assets/images/united-states.svg') }}" alt="USA">
-                                                <span>(+1) 630-339-2658</span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <!-- Page Contact Item End -->
-
-                            <!-- Page Contact Item Start -->
-                            <div class="contact-info-item wow fadeInUp" data-wow-delay="0.25s">
-                                <div class="icon-box">
-                                    <img src="{{ asset('assets/images/icon-mail.svg') }}" alt="">
-                                </div>
-                                <div class="contact-info-content">
-                                    <h3>Email</h3>
-                                    <p>Info@hanudaglobal.com</p>
-                                </div>
-                            </div>
-                            <!-- Page Contact Item End -->
-
-                            <!-- Page Contact Item Start -->
-                            <div class="contact-info-item wow fadeInUp" data-wow-delay="0.5s">
-                                <div class="icon-box">
-                                    <img src="{{ asset('assets/images/icon-location.svg') }}" alt="">
-                                </div>
-                                <div class="contact-info-content">
-                                    <h3>Our Address</h3>
-                                    <p>17 McEwan Dr West, Bolton
-                                        Ontario, Canada L7E1H5</p>
-                                </div>
-                            </div>
-                            <!-- Page Contact Item End -->
-                        </div>
 <!-- Page Contact Us Start -->
 <div class="page-contact-us">
     <div class="container">
@@ -151,8 +78,9 @@
                         </div>
                         <!-- Page Contact Item End -->
                     </div>
-                    <!-- Contact Information End -->
                 </div>
+                <!-- Contact Information End -->
+            </div>
 
             <div class="col-lg-7">
                 <!-- Page Contact Form Start -->
@@ -211,35 +139,35 @@
                         </form>
                         <!-- Contact Form End -->
                     </div>
-                    <!-- Page Contact Form End -->
                 </div>
+                <!-- Page Contact Form End -->
             </div>
         </div>
     </div>
+</div>
 
-    @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-    <!--  Page Contact Us End -->
+@if(session('success'))
 
-    <!-- Google Map Start -->
-    <div class="google-map">
-        <div class="container-flude">
-            <div class="row">
-                <div class="col-lg-12">
-                    <!-- Google Map Start -->
-                    <div class="google-map-iframe">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2876.825695196549!2d-79.71865162336164!3d43.85944133906663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b222072c3e65d%3A0x79ec37af23b72a3e!2s17%20McEwan%20Dr%20W%2C%20Bolton%2C%20ON%20L7E%201H5%2C%20Canada!5e0!3m2!1sen!2sin!4v1782730677803!5m2!1sen!2sin"
-                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="strict-origin-when-cross-origin"></iframe>
-                    </div>
-                    <!-- Google Map End -->
+<div class="alert alert-success">
+    {{ session('success') }}
+</div>
+
+@endif
+<!--  Page Contact Us End -->
+
+<!-- Google Map Start -->
+<div class="google-map">
+    <div class="container-flude">
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Google Map Start -->
+                <div class="google-map-iframe">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2876.825695196549!2d-79.71865162336164!3d43.85944133906663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b222072c3e65d%3A0x79ec37af23b72a3e!2s17%20McEwan%20Dr%20W%2C%20Bolton%2C%20ON%20L7E%201H5%2C%20Canada!5e0!3m2!1sen!2sin!4v1782730677803!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
                 </div>
+                <!-- Google Map End -->
             </div>
         </div>
     </div>
-    <!-- Google Map End -->
+</div>
+<!-- Google Map End -->
 @endsection
